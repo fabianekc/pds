4314e69e5ea1d1093a1a9fce175fc4abfe5a6f8c
Christoph Fabianek <christoph.fabianek@gmail.com>
2015-03-08 22:54:58 +0100

    fix production.rb 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-pds::Application.configure do
+Pds::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
