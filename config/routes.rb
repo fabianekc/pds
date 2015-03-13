34cce4671c7f054c6900b9530e6fb58d458a8ad4
Christoph Fabianek <christoph.fabianek@gmail.com>
2015-03-13 20:50:58 +0100

    app.json tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get "message/show"
-  root 'static#hello'
+  scope "(:locale)", :locale => /en|de/ do
+    root  'static#hello'
+    match '/configure', to: 'static#hello', via: 'get'
+  end  
 end
