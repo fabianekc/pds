4db401843d8b6297dbc0f00cef30f1c973f05286
Christoph Fabianek <christoph.fabianek@gmail.com>
2015-03-14 01:14:08 +0100

    turn of forgery 2

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,6 +1,7 @@
 module Api
   class BaseController < ApplicationController
 #    protect_from_forgery with: :null_session
+    skip_before_filter :verify_authenticity_token
     before_action :set_resource, only: [:destroy, :show, :update]
     respond_to :json
 
