54940709e36b7d634a707fe40389b245c6337bc1
Christoph Fabianek <christoph.fabianek@gmail.com>
2015-04-09 14:13:17 +0200

    repo list 2

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -3,17 +3,20 @@ class ReposController < ApplicationController
   def index
     @available = Repository.all.pluck(:vat)
     @unknowns = Response.where.not(vat: Repository.pluck(:vat)).pluck(:vat).uniq
-    @repos = (@available + @unknowns) #.paginate(page: params[:page], per_page: 15)
+    @repos = (@available + @unknowns).paginate(page: params[:page], per_page: 15)
   end
 
   def show
     @repo = Repository.find_by_vat(params[:id])
     @responses = Response.where(vat: params[:id])
     if @repo.nil?
+      server = @responses.first.list.to_s
+      if server == "" then
+        server = "https://www.crwdst.at"
+      end
       puts "get======================"
-      puts @responses.first.list + "/" + params[:id] + "/quest_json"
-      lookup = HTTParty.get(@responses.first.list + "/" +
-        params[:id] + "/quest_json")
+      puts server + "/" + params[:id] + "/quest_json"
+      lookup = HTTParty.get(server + "/" + params[:id] + "/quest_json")
       if lookup.success?
         @repo = Repository.create(name: lookup[0]['name'],
                                   vat:  params[:id])
