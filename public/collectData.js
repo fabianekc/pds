8936894255d8f6fb0c55321bf814c34c0a757a66
Christoph Fabianek <christoph.fabianek@gmail.com>
2015-04-12 22:34:23 +0200

    external collectData.js 2

--- public/collectData.js
+++ public/collectData.js
@@ -1,12 +1,12 @@
 function collectData(pds, quest_id) {
-  $("#searchForm").submit(function(event) {
+  document.getElementById('searchForm').submit(function(event) {
     event.preventDefault();
-    var term = $("#searchField").val();
+    var term = document.getElementById('searchField').val();
     var url = pds + "/api/responses";
     $.ajax({url: url, dataType: 'jsonp',
       data: { response: { value: term, vat: quest_id } }
     });
-    $("#searchForm").hide();
-    $("#result").innerHTML("done");
+    document.getElementById('searchForm').hide();
+    document.getElementById('result').innerHTML("done");
   });
 }
